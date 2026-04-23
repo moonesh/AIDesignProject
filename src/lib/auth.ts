037a3e2c4f9a6e091bbda3b233bddedb2bd95744
Moonesh Kachroo <moonesh.kachroo@hclhealthcare.in>
2026-04-23 12:00:37 +0530

    Commit three - just a test

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
 import { db } from "./db"
-
+//Test
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
   database: drizzleAdapter(db, {
